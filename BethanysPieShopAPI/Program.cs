29818b0ed4655424304933c1d44632c47c30b95d
kristina-vezba <4kristinabegovic@gmail.com>
2022-12-15 12:33:34 +0100

    Popravljeno "failed to load api definition"

--- BethanysPieShopAPI/Program.cs
+++ BethanysPieShopAPI/Program.cs
@@ -1,6 +1,7 @@
 using BethanysPieShopAPI.DbContexts;
 using BethanysPieShopAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,8 +18,10 @@ builder.Services.AddControllers(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BethanysPieShopAPI", Version = "v1" });
+});
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
     options.UseSqlServer(builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 });
@@ -32,8 +35,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwaggerUI();
-    app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BethanysPieShopAPI");
+    });
 }
 
 app.UseHttpsRedirection();
